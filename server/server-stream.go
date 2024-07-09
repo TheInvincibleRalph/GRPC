e40a6781caee10ee4c994000752aad22b28f8338
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-07-09 02:51:04 +0100

    initial message

--- server/server-stream.go
+++ server/server-stream.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"time"
+
 	pb "github.com/theinvincible/grpc/proto"
 
 	"log"
 )
 
-func (s *helloServer) callSayHelloServerStreaming(req *pb.NamesList, steam pb.GreetService_SayHelloServerStreamingServer) error {
+func (s *helloServer) callSayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
 	log.Printf("got request with names : %v", req.Names)
 	for _, name := range req.Names {
 
@@ -16,5 +18,7 @@ func (s *helloServer) callSayHelloServerStreaming(req *pb.NamesList, steam pb.Gr
 		if err := stream.Send(res); err != nil {
 			return err
 		}
+		time.Sleep(2 * time.Second)
 	}
+	return nil
 }
